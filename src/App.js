3ff8aa20f85be67f7441c0d249d8ef2dd337da4d
Nate <natemausert@gmail.com>
2022-10-10 13:36:22 -0700

    add copyright and dark theme

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './styles/App.css';
 
+import { MantineProvider } from '@mantine/core';
+import { Affix } from '@mantine/core';
 import Landing from "./components/Landing";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+      <MantineProvider
+      theme={{
+        colorScheme: 'dark',
+        fontFamily: 'Open Sans, sans serif',
+        spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 }
+      }}>
+      <Affix position={{ bottom: 20, right: 20 }}>
+        <p>Copyright 2022 Darkmeow productions</p>
+      </Affix>
+    </MantineProvider>
   );
 }
 
