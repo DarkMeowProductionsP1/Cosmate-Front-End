9234e48917f92aef6329d599a93838effa705898
Nate <natemausert@gmail.com>
2022-10-10 13:23:15 -0700

    stub up components, start adding mantine basics

---
+++ src/components/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { MantineProvider, NavLink } from '@mantine/core';
+
+const Landing = (props) => {
+
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default Landing
